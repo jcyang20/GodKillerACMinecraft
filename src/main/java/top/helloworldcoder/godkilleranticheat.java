539149184dadd0ff066a89396201c5899a7a365e
HelloWorldCoder-China <HelloWorldCoder-China@outlook.com>
2025-05-25 13:32:01 +0800

    初始化Git
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/top/helloworldcoder/godkilleranticheat.java
@@ -0,0 +1,19 @@
+package top.helloworldcoder;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class godkilleranticheat extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
