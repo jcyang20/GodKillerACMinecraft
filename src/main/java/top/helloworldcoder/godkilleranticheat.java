63e28e55d51816a82215a1a2c37d59b16a32bb04
HelloWorldCoder <HelloWorldCoder-China@outlook.com>
2025-05-25 20:52:36 +0800

    开头提交，没啥东西的

--- src/main/java/top/helloworldcoder/godkilleranticheat.java
+++ src/main/java/top/helloworldcoder/godkilleranticheat.java
@@ -1,15 +1,23 @@
 package top.helloworldcoder;
 
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class godkilleranticheat extends JavaPlugin
+import java.util.logging.Level;
+
+public final class godkilleranticheat extends JavaPlugin implements Listener
 {
 
     @Override
     public void onEnable()
     {
         // Plugin startup logic
-
+        getLogger().log(Level.INFO,"插件起动中...");
+        getLogger().log(Level.INFO,"启动事件监听...");
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().log(Level.INFO,"监听启动完成");
+        getLogger().log(Level.INFO,"加载配置文件...");
+        getLogger().log(Level.INFO,"插件启动完成");
     }
 
     @Override
