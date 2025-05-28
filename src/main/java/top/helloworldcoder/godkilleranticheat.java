0460ae79b0a340d26fe3bb3a4b05ccee00f98679
HelloWorldCoder <HelloWorldCoder-China@outlook.com>
2025-05-28 20:56:04 +0800

    test VSCode

--- src/main/java/top/helloworldcoder/godkilleranticheat.java
+++ src/main/java/top/helloworldcoder/godkilleranticheat.java
@@ -2,6 +2,7 @@ package top.helloworldcoder;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
@@ -27,18 +28,18 @@ public final class godkilleranticheat extends JavaPlugin implements Listener
         getLogger().log(Level.INFO,"插件启动完成");
     }
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) //封禁处理 2025/5/27 13:38 Commit No.1
+    public void onAsyncLogin(AsyncPlayerPreLoginEvent event) //封禁处理 2025/5/27 13:38 Commit No.1
     {
         for(List<String> list:banlist)
         {   // 判断玩家是否被封禁，目前遍历，不知道大佬们有没有更好的解决方法
-            if(list.get(0).equals(event.getPlayer().getName()) || list.get(1).equals(Objects.requireNonNull(event.getPlayer().getAddress()).getAddress().toString()))
+            if(list.get(0).equals(event.getName()) || list.get(1).equals(Objects.requireNonNull(event.getAddress()).getAddress().toString()))
             {
                 // 到期判断，记得添加 到期判断，记得添加 到期判断，记得添加 到期判断，记得添加 到期判断，记得添加 到期判断，记得添加
                 // 到期判断，记得添加 到期判断，记得添加 到期判断，记得添加 到期判断，记得添加 到期判断，记得添加 到期判断，记得添加
                 // 到期判断，记得添加 到期判断，记得添加 到期判断，记得添加 到期判断，记得添加 到期判断，记得添加 到期判断，记得添加
-                event.getPlayer().kickPlayer(chatprefix+"\n诛仙!\n你被封印了,理由: "+list.get(3)+"剩余封印时间: "+list.get(2)); // 目前使用踢出，占用较大，且有漏洞风险，不知道大佬们有没有类似TempBan那种解决方案可以用
+                event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_BANNED,chatprefix+"\n诛仙!\n你被封印了,理由: "+list.get(3)+"剩余封印时间: "+list.get(2)); // 目前使用踢出，占用较大，且有漏洞风险，不知道大佬们有没有类似TempBan那种解决方案可以用
                 // 封禁信息提示至log
-                getLogger().log(Level.INFO,"被封印玩家"+event.getPlayer().getName()+"被踢出了服务器,理由: "+list.get(3)+"剩余封印时间: "+list.get(2));
+                getLogger().log(Level.INFO,"被封印玩家"+event.getName()+"被踢出了服务器,理由: "+list.get(3)+"剩余封印时间: "+list.get(2));
             }
         }
     }
