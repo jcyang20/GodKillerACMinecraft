9a67e48932923aac5e4b2cc6ba701babed655627
HelloWorldCoder <HelloWorldCoder-China@outlook.com>
2025-05-30 23:27:20 +0800

    日志颜色系统就绪,参考了SkinRestorer(不是抄袭,我看起来像雀德的人吗?)日志系统打包成的外接库,有需要的从源代码里爬出来自取,使用Minecraft原版颜色代码,还有一些新的,自行探索!!不要删除版权声明!! =￣ω￣= 最后,感谢大家的支持!!! ヾ(≧▽≦*)o

--- src/main/java/DragonUtils/logging.java
+++ src/main/java/DragonUtils/logging.java
@@ -5,7 +5,11 @@ import static org.bukkit.Bukkit.getLogger;
 
 public final class logging
 {
-    public static String ANSIDataBase(char colorcode)
+    static
+    {
+        logging.log(Level.INFO,"&6[DragonUtils] &r","&aSuccessfully Loaded &r&6&_DragonUtils by HelloWorldCoder-China&r&a !&r");
+    }
+    public static String ANSIDataBase(char colorcode) // ANSI数据库,手打
     {
         if(colorcode=='0') return "\u001B[30;22m";
         if(colorcode=='1') return "\u001B[34;22m";
@@ -15,25 +19,37 @@ public final class logging
         if(colorcode=='5') return "\u001B[35;22m";
         if(colorcode=='6') return "\u001B[33;22m";
         if(colorcode=='7') return "\u001B[37;22m";
-        if(colorcode=='8') return "\u001B[30;1m";
-        if(colorcode=='9') return "\u001B[34;1m";
-        if(colorcode=='a') return "\u001B[32;1m";
-        if(colorcode=='b') return "\u001B[36;1m";
-        if(colorcode=='c') return "\u001B[31;1m";
-        if(colorcode=='d') return "\u001B[35;1m";
-        if(colorcode=='e') return "\u001B[33;1m";
-        if(colorcode=='f') return "\u001B[37;1m";
+        if(colorcode=='8') return "\u001B[30;22m";
+        if(colorcode=='9') return "\u001B[34;22m";
+        if(colorcode=='a') return "\u001B[32;22m";
+        if(colorcode=='b') return "\u001B[36;22m";
+        if(colorcode=='c') return "\u001B[31;22m";
+        if(colorcode=='d') return "\u001B[35;22m";
+        if(colorcode=='e') return "\u001B[33;22m";
+        if(colorcode=='f') return "\u001B[37;22m";
         if(colorcode=='k') return "\u001B[5m";
-        if(colorcode=='l') return "\u001B[21m";
+        if(colorcode=='l') return "\u001B[1m";
         if(colorcode=='m') return "\u001B[9m";
         if(colorcode=='n') return "\u001B[4m";
         if(colorcode=='o') return "\u001B[3m";
-        if(colorcode=='r') return "\u001B[0;39m";
-        return "\u001B[31;22m\u001B[21m[ERROR]\u001B[0;39m\u001B[31;1m Color Code NOT FOUND: "+colorcode+"!";
+        if(colorcode=='r') return "\u001B[0m\u001B[0;39m\u001B[24m";
+        if(colorcode=='_') return "\u001B[21m";
+        if(colorcode=='-') return "&";
+        return "\u001B[31;22m\u001B[21m[ERROR]\u001B[0;39m\u001B[31;22m Color Code NOT FOUND: "+colorcode+"!\u001B[0m\u001B[0;39m\u001B[24m";
     }
-    public static boolean log(Level level,String prefix,String message)
+    public static String ANSIChange(String Source) // ANSI转换器
     {
-        getLogger().log(level,prefix+message);
+        Source=Source.replaceAll("&","§"); // 初始化字符串
+        while(Source.contains("§")) // 查找
+        {
+            char colorcode=Source.charAt(Source.indexOf("§")+1);
+            Source=Source.replaceAll("§"+colorcode,ANSIDataBase(colorcode)); // 替换
+        }
+        return Source;
+    }
+    public static boolean log(Level level,String prefix,String message) // 日志输出
+    {
+        getLogger().log(level,ANSIChange(prefix+message+"&r"));
         return true;
     }
 }
